728f84773bc01d493d84ea5b873a915d4e2b6afb
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-11-08 09:26:01 -0500

    Add StorageService to persist data
    Previously it was the dummy implementation, not we actually store
    things like user preferences to disk.
    
    Hive is supposed to be very fast, and shows off the encryption
    capabilities. Other non-encrypted options are definitely available
    if desired.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 /// A fast and beautiful app to help convey emotion in text communication.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,9 +9,16 @@ import 'src/emoji/emoji.json.dart';
 import 'src/emoji/emoji_service.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+import 'src/storage/storage_service.dart';
 
 void main() async {
-  final settingsController = SettingsController(SettingsService());
+  // Initialize the storage service.
+  final storageService = StorageService();
+  await storageService.init();
+
+  // Initialize the settings service.
+  final settingsService = SettingsService(storageService);
+  final settingsController = SettingsController(settingsService);
 
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
