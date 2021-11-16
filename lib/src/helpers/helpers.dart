48d6ad7fa99629f7a45110776690eb05996f977f
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2021-11-16 14:23:24 -0500

    Add web support

---
+++ lib/src/helpers/helpers.dart
@@ -0,0 +1,31 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+/// Convenience function to check if the app is running on a desktop computer.
+///
+/// If the currently running platform is one of Linux, Windows, or MacOS
+/// this returns true.
+///
+/// If the running platform is Android, iOS or Web this returns false.
+bool platformIsDesktop() {
+  final bool platformIsDesktop;
+  if (kIsWeb) {
+    platformIsDesktop = false;
+  } else {
+    switch (Platform.operatingSystem) {
+      case 'linux':
+        platformIsDesktop = true;
+        break;
+      case 'windows':
+        platformIsDesktop = true;
+        break;
+      case 'macos':
+        platformIsDesktop = true;
+        break;
+      default:
+        platformIsDesktop = false;
+    }
+  }
+  return platformIsDesktop;
+}
