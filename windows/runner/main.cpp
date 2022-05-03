5be046bce6b42b0d6f91b576e427af65b7432fea
Yaroslav Pronin <proninyaroslav@gmail.com>
2022-05-03 17:17:36 +0300

    [Desktop/Web] Fix window title

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,6 +5,8 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+const wchar_t *WINDOW_TITLE = L"Feeling Finder";
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   // Attach to console when present (e.g., 'flutter run') or create a
@@ -27,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(620, 590);
-  if (!window.CreateAndShow(L"feeling_finder", origin, size)) {
+  if (!window.CreateAndShow(WINDOW_TITLE, origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
