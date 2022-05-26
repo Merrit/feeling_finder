f36cc823f2d28b465fa5c585636fc52e1f4ad128
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2022-05-26 14:28:14 -0400

    Linux: close existing sessions at startup #48

---
+++ lib/src/helpers/close_exiting_sessions.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+/// An issue was raised where the error indicated the app couldn't
+/// launch because the hive database was already locked, so we will
+/// kill any existing version of the app before continuing to load.
+Future<void> closeExistingSessions() async {
+  if (Platform.isLinux) await _closeOnLinux();
+}
+
+Future<void> _closeOnLinux() async {
+  final result = await Process.run(
+    'bash',
+    ['-c', 'ps -A | grep feeling_finder'],
+  );
+
+  final processLines = (result.stdout as String).trim().split('\n');
+
+  for (var processLine in processLines) {
+    final process = _LinuxProcessResult.fromTerminalOutput(processLine);
+    if (process.pid == pid) continue;
+    if (process.executableName != 'feeling_finder') continue;
+    debugPrint('Closing existing instance with pid ${process.pid}');
+    Process.killPid(process.pid);
+  }
+}
+
+class _LinuxProcessResult {
+  final int pid;
+  final String executableName;
+
+  const _LinuxProcessResult({
+    required this.pid,
+    required this.executableName,
+  });
+
+  factory _LinuxProcessResult.fromTerminalOutput(String terminalLine) {
+    final List<String> pieces = terminalLine.trim().split(' ');
+    return _LinuxProcessResult(
+      pid: int.parse(pieces.first),
+      executableName: pieces.last,
+    );
+  }
+}
