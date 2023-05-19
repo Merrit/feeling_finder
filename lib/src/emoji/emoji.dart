40d4c55635b955b3b25a5e59c75272e74223e97e
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-05-19 12:11:44 -0400

    refactor: emoji model to freezed

--- lib/src/emoji/emoji.dart
+++ lib/src/emoji/emoji.dart
@@ -1,83 +1,39 @@
-import 'dart:convert';
-
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'emoji_category.dart';
+
+part 'emoji.freezed.dart';
+part 'emoji.g.dart';
 
 /// Represents an emoji.
 ///
 /// Also contains associated metadata like description and category.
-class Emoji extends Equatable {
-  /// The actual emoji, for example: 😆
-  final String emoji;
+@freezed
+class Emoji with _$Emoji {
+  const factory Emoji({
+    /// A list of strings that also match this emoji from the unicode spec.
+    /// Example: ```["laughing", "satisfied"]```.
+    required List<String> aliases,
 
-  /// The emoji's description from the unicode spec.
-  /// Example: "grinning squinting face".
-  final String description;
+    /// The emoji's category from the unicode spec.
+    /// Example: "Smileys & Emotion".
+    required EmojiCategory category,
 
-  /// The emoji's category from the unicode spec.
-  /// Example: "Smileys & Emotion".
-  final EmojiCategory category;
+    /// The emoji's description from the unicode spec.
+    /// Example: "grinning squinting face".
+    required String description,
 
-  /// A list of strings that also match this emoji from the unicode spec.
-  /// Example: ```["laughing", "satisfied"]```.
-  final List<String> aliases;
+    /// The actual emoji, for example: 😆
+    required String emoji,
 
-  /// Another list of strings that also match this emoji from the unicode spec.
-  /// Example: ```["happy", "haha"]```.
-  final List<String> tags;
+    /// Another list of strings that also match this emoji from the unicode spec.
+    /// Example: ```["happy", "haha"]```.
+    required List<String> tags,
 
-  /// The emoji's version per the unicode spec.
-  /// Example: "6.0".
-  final String unicodeVersion;
+    /// The emoji's version per the unicode spec.
+    /// Example: "6.0".
+    required String unicodeVersion,
+  }) = _Emoji;
 
-  const Emoji({
-    required this.emoji,
-    required this.description,
-    required this.category,
-    required this.aliases,
-    required this.tags,
-    required this.unicodeVersion,
-  });
-
-  /// Builds an Emoji from the provided [json].
-  factory Emoji.fromJson(Map<String, dynamic> json) {
-    EmojiCategory emojiCategory = emojiCategoryFromString(
-      json['category'] as String,
-    );
-
-    return Emoji(
-      emoji: json['emoji'] as String,
-      description: json['description'] as String,
-      category: emojiCategory,
-      aliases: json['aliases'].cast<String>(),
-      tags: json['tags'].cast<String>(),
-      unicodeVersion: json['unicode_version'] as String,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'emoji': emoji,
-      'description': description,
-      'category': category.name,
-      'aliases': aliases,
-      'tags': tags,
-      'unicode_version': unicodeVersion,
-    };
-  }
-
-  String toJson() => json.encode(toMap());
-
-  @override
-  List<Object> get props {
-    return [
-      emoji,
-      description,
-      category,
-      aliases,
-      tags,
-      unicodeVersion,
-    ];
-  }
+  factory Emoji.fromJson(Map<String, dynamic> json) => _$EmojiFromJson(json);
 }
