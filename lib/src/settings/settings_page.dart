9e0ec6c19be381cc3ef595bf9a10ec3cd1fe1dce
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-06-18 15:37:35 -0400

    refactor: settings_page

--- lib/src/settings/settings_page.dart
+++ lib/src/settings/settings_page.dart
@@ -25,6 +25,141 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final settingsCubit = context.read<SettingsCubit>();
 
+    // Glue the SettingsCubit to the theme selection DropdownButton.
+    //
+    // When a user selects a theme from the dropdown list, the
+    // SettingsCubit is updated, which rebuilds the MaterialApp.
+    final Widget themeTile = BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return ListTile(
+          title: Text(translations.settings.theme),
+          trailing: DropdownButton<ThemeMode>(
+            // Read the selected themeMode from the controller
+            value: state.userThemePreference,
+            // Call the updateThemeMode method any time the user selects a theme.
+            onChanged: settingsCubit.updateThemeMode,
+            items: [
+              DropdownMenuItem(
+                value: ThemeMode.system,
+                child: Text(
+                  translations.settings.systemTheme,
+                ),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.light,
+                child: Text(translations.settings.lightTheme),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.dark,
+                child: Text(translations.settings.darkTheme),
+              )
+            ],
+          ),
+        );
+      },
+    );
+
+    final Widget exitAfterCopyTile = BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          title: Text(translations.settings.exitAfterCopy),
+          value: state.exitOnCopy,
+          onChanged: (value) => settingsCubit.updateExitOnCopy(value),
+        );
+      },
+    );
+
+    final Widget showTrayTile = BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          title: Text(translations.settings.showSystemTray),
+          value: state.showSystemTrayIcon,
+          onChanged: (value) => settingsCubit.updateShowSystemTrayIcon(value),
+        );
+      },
+    );
+
+    final Widget hotkeyEnabledTile = BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return SwitchListTile(
+          title: Text(translations.settings.hotkeyToggle),
+          value: state.hotKeyEnabled,
+          onChanged: (value) {
+            if (value) {
+              hotKeyService.initHotkeyRegistration();
+            } else {
+              hotKeyService.unregisterBindings();
+            }
+            settingsCubit.updateHotKeyEnabled(value);
+          },
+        );
+      },
+    );
+
+    final Widget hotkeyConfigurationTile = BlocBuilder<SettingsCubit, SettingsState>(
+      builder: (context, state) {
+        return Visibility(
+            visible: state.hotKeyEnabled,
+            maintainAnimation: true,
+            maintainState: true,
+            child: AnimatedOpacity(
+              opacity: state.hotKeyEnabled ? 1.0 : 0.0,
+              duration: const Duration(milliseconds: 500),
+              //TODO: Replace with proper hotkey configuration
+              child: Text(translations.settings.shortcutUsage(
+                  modifierKey: KeyModifier.alt.keyLabel, actionKey: KeyCode.period.keyLabel)),
+            ));
+      },
+    );
+
+    final Widget versionWidgets = BlocBuilder<AppCubit, AppState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            ListTile(
+              title: Text(
+                '${translations.settings.currentVersion}: ${state.runningVersion}',
+              ),
+            ),
+            if (state.updateVersion != null)
+              ListTile(
+                title: Text(
+                  (state.updateAvailable)
+                      ? '${translations.settings.updateAvailable}: ${state.updateVersion}'
+                      : translations.settings.upToDate,
+                ),
+              ),
+            ListTile(
+              title: const Text('About'),
+              onTap: () => showAboutDialog(
+                context: context,
+                applicationIcon: CircleAvatar(
+                  child: Image.asset('assets/icons/codes.merritt.FeelingFinder.png'),
+                ),
+                applicationName: 'Feeling Finder',
+                applicationVersion: state.runningVersion,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+
+    final Widget homepageTile = ListTile(
+      title: Text(translations.settings.homepage),
+      trailing: const Icon(Icons.language),
+      onTap: () => AppCubit.instance.launchURL(kWebsiteUrl),
+    );
+
+    final Widget donateTile = ListTile(
+      title: Text(translations.settings.donate),
+      trailing: const Icon(
+        Icons.favorite,
+        color: Colors.red,
+      ),
+      onTap: () => AppCubit.instance.launchURL(kDonateUrl),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(translations.settings.title),
@@ -35,156 +170,17 @@ class SettingsPage extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(height: 50),
-
-              // Glue the SettingsCubit to the theme selection DropdownButton.
-              //
-              // When a user selects a theme from the dropdown list, the
-              // SettingsCubit is updated, which rebuilds the MaterialApp.
-              BlocBuilder<SettingsCubit, SettingsState>(
-                builder: (context, state) {
-                  return ListTile(
-                    title: Text(translations.settings.theme),
-                    trailing: DropdownButton<ThemeMode>(
-                      // Read the selected themeMode from the controller
-                      value: state.userThemePreference,
-                      // Call the updateThemeMode method any time the user selects a theme.
-                      onChanged: settingsCubit.updateThemeMode,
-                      items: [
-                        DropdownMenuItem(
-                          value: ThemeMode.system,
-                          child: Text(
-                            translations.settings.systemTheme,
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: ThemeMode.light,
-                          child: Text(translations.settings.lightTheme),
-                        ),
-                        DropdownMenuItem(
-                          value: ThemeMode.dark,
-                          child: Text(translations.settings.darkTheme),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-
+              themeTile,
               const Divider(),
-
-              BlocBuilder<SettingsCubit, SettingsState>(
-                builder: (context, state) {
-                  return SwitchListTile(
-                    title: Text(translations.settings.exitAfterCopy),
-                    value: state.exitOnCopy,
-                    onChanged: (value) => settingsCubit.updateExitOnCopy(value),
-                  );
-                },
-              ),
-
+              exitAfterCopyTile,
+              if (defaultTargetPlatform.isDesktop) showTrayTile,
+              if (runsX11) hotkeyEnabledTile,
+              if (runsX11) hotkeyConfigurationTile,
               const Divider(),
-
-              if (defaultTargetPlatform.isDesktop)
-                BlocBuilder<SettingsCubit, SettingsState>(
-                  builder: (context, state) {
-                    return SwitchListTile(
-                      title: Text(translations.settings.showSystemTray),
-                      value: state.showSystemTrayIcon,
-                      onChanged: (value) => settingsCubit.updateShowSystemTrayIcon(value),
-                    );
-                  },
-                ),
-
-              if (runsX11)
-                BlocBuilder<SettingsCubit, SettingsState>(
-                  builder: (context, state) {
-                    return SwitchListTile(
-                      title: Text(translations.settings.hotkeyToggle),
-                      value: state.hotKeyEnabled,
-                      onChanged: (value) {
-                        if (value) {
-                          hotKeyService.initHotkeyRegistration();
-                        } else {
-                          hotKeyService.unregisterBindings();
-                        }
-                        settingsCubit.updateHotKeyEnabled(value);
-                      },
-                    );
-                  },
-                ),
-
-              if (runsX11)
-                BlocBuilder<SettingsCubit, SettingsState>(
-                  builder: (context, state) {
-                    return Visibility(
-                        visible: state.hotKeyEnabled,
-                        maintainAnimation: true,
-                        maintainState: true,
-                        child: AnimatedOpacity(
-                          opacity: state.hotKeyEnabled ? 1.0 : 0.0,
-                          duration: const Duration(milliseconds: 500),
-                          //TODO: Replace with proper hotkey configuration
-                          child: Text(translations.settings.shortcutUsage(
-                              modifierKey: KeyModifier.alt.keyLabel,
-                              actionKey: KeyCode.period.keyLabel)),
-                        ));
-                  },
-                ),
-
-              if (runsX11) const Divider(),
-
-              BlocBuilder<AppCubit, AppState>(
-                builder: (context, state) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        title: Text(
-                          '${translations.settings.currentVersion}: ${state.runningVersion}',
-                        ),
-                      ),
-                      if (state.updateVersion != null)
-                        ListTile(
-                          title: Text(
-                            (state.updateAvailable)
-                                ? '${translations.settings.updateAvailable}: ${state.updateVersion}'
-                                : translations.settings.upToDate,
-                          ),
-                        ),
-                      ListTile(
-                        title: const Text('About'),
-                        onTap: () => showAboutDialog(
-                          context: context,
-                          applicationIcon: CircleAvatar(
-                            child: Image.asset('assets/icons/codes.merritt.FeelingFinder.png'),
-                          ),
-                          applicationName: 'Feeling Finder',
-                          applicationVersion: state.runningVersion,
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-
+              versionWidgets,
               const Divider(),
-
-              Column(
-                children: [
-                  ListTile(
-                    title: Text(translations.settings.homepage),
-                    trailing: const Icon(Icons.language),
-                    onTap: () => AppCubit.instance.launchURL(kWebsiteUrl),
-                  ),
-                  ListTile(
-                    title: Text(translations.settings.donate),
-                    trailing: const Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                    ),
-                    onTap: () => AppCubit.instance.launchURL(kDonateUrl),
-                  ),
-                ],
-              ),
+              homepageTile,
+              donateTile,
             ],
           ),
         ),
